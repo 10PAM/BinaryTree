27929ae1d1181b76e0440b8ca3787f7cc554a695
MAP01 <maguilera131113@gmail.com>
2026-09-20 03:49:42 -0700

    Update main.java

--- vJava/main.java
+++ vJava/main.java
@@ -4,5 +4,7 @@ About: Driver class for binary tree. Creates tree.
 */
 
 public static void main(String[] args) {
+  int arrayRepresentation = {1, 2, 3};
   
+  BinaryTree binTreeA = new BinaryTree(arrayRepresentation);
 }
