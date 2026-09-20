2fb433b292819913d52329fa2236108b5bed0c0c
MAP01 <maguilera131113@gmail.com>
2026-09-20 04:25:44 -0700

    Include binary-tree header and initialize BinaryTree

--- vCPP/main.cpp
+++ vCPP/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "binary-tree.h"
 
 int main() {
   std::cout << "In Order (IN); Level order (LO); " << std::endl;
-  
+
+  int binArray[] = {1, 2, 3};
+  BinaryTree tree = new BinaryTree(binArray);
 }
