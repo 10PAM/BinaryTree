913e9159137714ad32bcf1212d21dde3d1b17c01
MAP01 <maguilera131113@gmail.com>
2026-09-22 05:29:21 -0700

    Add header comments to main.cpp
    Added header comments with programmer and AI details.

--- vCPP/main.cpp
+++ vCPP/main.cpp
@@ -1,3 +1,20 @@
+/*
+
+Programmers {
+  1. Mario Aguilera Piceno
+}
+
+AI Utilized {
+  1. Gemini - Used to help guide the intial in-order recursive helper methods.
+}
+
+File: main.cpp
+About: BinaryTree program entry point.
+Created: 09/21/2026
+Updated: 09/22/2026
+
+*/
+
 #include <iostream>
 #include "binary-tree.h"
 
