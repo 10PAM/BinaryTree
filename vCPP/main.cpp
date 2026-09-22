8527e303b19eb8dbb6af908b07cfa1723367785d
MAP01 <maguilera131113@gmail.com>
2026-09-22 05:21:51 -0700

    Update main.cpp

--- vCPP/main.cpp
+++ vCPP/main.cpp
@@ -2,8 +2,11 @@
 #include "binary-tree.h"
 
 int main() {
+  // Ask user to provide values for tree:
   std::cout << "In Order (IN); Level order (LO); " << std::endl;
-
+  
   int binArray[] = {1, 2, 3};
+  
   BinaryTree tree = new BinaryTree(binArray);
+  
 }
