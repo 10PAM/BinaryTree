f9e63607d9c7a5c562dcb18da15e86966e64ace9
MAP01 <maguilera131113@gmail.com>
2026-09-23 10:36:47 -0700

    Update binary-tree.h

--- vCPP/binary-tree.h
+++ vCPP/binary-tree.h
@@ -4,14 +4,10 @@ Programmers {
   1. Mario Aguilera Piceno
 }
 
-AI Utilized {
-  1. Gemini - Used to help guide the intial in-order recursive helper methods.
-}
-
 File: binary-tree.h
 About: Handles BinaryTree class once created from main.cpp.
 Created: 09/21/2026
-Updated: 09/22/2026
+Updated: 09/23/2026
 
 */
 
